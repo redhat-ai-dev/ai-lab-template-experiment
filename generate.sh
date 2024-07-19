b854ab3632205edf055fc2d5e51e47eedb4b6f18
Jordan Dubrick <jdubrick@redhat.com>
2024-07-19 16:43:25 -0400

    pull in new script changes and update deployment info
    Signed-off-by: Jordan Dubrick <jdubrick@redhat.com>

--- generate.sh
+++ generate.sh
@@ -1,5 +1,8 @@
 ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
 
+# Flags scripts to use experiment branch for the samples
+export SAMPLE_BRANCH=experiment
+
 # get app templates
 $ROOTDIR/scripts/update-tekton-definition
 $ROOTDIR/scripts/import-gitops-template
