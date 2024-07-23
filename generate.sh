5a3d68c44160daf865e6a43ba437ac76ae5cf597
Jordan Dubrick <jdubrick@redhat.com>
2024-07-23 13:38:11 -0400

    move env var setting to samples script
    Signed-off-by: Jordan Dubrick <jdubrick@redhat.com>

--- generate.sh
+++ generate.sh
@@ -1,8 +1,5 @@
 ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
 
-# Flags scripts to use experiment branch for the samples
-export SAMPLE_BRANCH=experiment
-
 # get app templates
 $ROOTDIR/scripts/update-tekton-definition
 $ROOTDIR/scripts/import-gitops-template
